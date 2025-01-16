62f9bc3285f60e06138532136500773b86b1db7d
Divya Crypto <katta.chowdary13@gmail.com>
2025-01-15 23:15:08 -0500

    Edited the first code

--- First.java
+++ First.java
@@ -1,7 +1,8 @@
-import java.util.*;
-Public class First{
+class First{
   public static void main(String[] args)
   {
     System.out.println("Hello World");
+    // ADD FUNCTION
+    System.out.println("Total amount added is:" + 26);
   }
 }
